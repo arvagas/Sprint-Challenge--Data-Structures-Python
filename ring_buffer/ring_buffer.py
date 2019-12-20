413758c7cb75220fd17a10692d3c1b30e31612b4
Arvin Agas <arvagas@gmail.com>
2019-12-20 12:08:55 -0500

    ring_buffer done could use some DRYer code

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -8,14 +8,37 @@ class RingBuffer:
         self.storage = DoublyLinkedList()
 
     def append(self, item):
+        if self.current is None:
+            self.current = 1
+
         if self.storage.length == self.capacity:
-            self.storage.remove_from_head()
-        self.storage.add_to_tail(item)
+            if self.current is 1:
+                self.storage.remove_from_head()
+                self.storage.add_to_head(item)
+                self.current += 1
+            elif self.current is 2:
+                self.storage.head.next.insert_after(item)
+                self.storage.head.next.delete()
+                self.current += 1
+            elif self.current is 3:
+                self.storage.head.next.next.insert_after(item)
+                self.storage.head.next.next.delete()
+                self.current += 1
+            elif self.current is 4:
+                self.storage.tail.prev.insert_before(item)
+                self.storage.tail.prev.delete()
+                self.current += 1
+            elif self.current is 5:
+                self.storage.remove_from_tail()
+                self.storage.add_to_tail(item)
+                self.current = 1
+
+        else:
+            self.storage.add_to_tail(item)
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
-
         # TODO: Your code here
         node = self.storage.head
         while node:
